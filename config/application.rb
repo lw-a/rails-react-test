f939c3eb6e0ef78b48a11601da8cfc221d3756de
Adam Hemming <hemming.dev@gmail.com>
2022-10-05 23:06:18 +0900

    create drink and ingredients model

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module RailsReactTest
     # Skip views, helpers and assets when generating a new resource.
 
     config.api_only = true
+    config.app_generators.scaffold_controller = :scaffold_controller
 
     # Middleware for ActiveAdmin
     config.middleware.use Rack::MethodOverride
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_05_133422) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_05_140356) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -35,4 +35,22 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_05_133422) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
+  create_table "drinks", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "steps"
+    t.string "source"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.integer "drink_id", null: false
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["drink_id"], name: "index_ingredients_on_drink_id"
+  end
+
+  add_foreign_key "ingredients", "drinks"
 end
