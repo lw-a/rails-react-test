e679d697e551f399f350514b372233922fbf0873
Adam Hemming <hemming.dev@gmail.com>
2022-10-05 23:12:21 +0900

    sort out routes

--- app/admin/ingredients.rb
+++ app/admin/ingredients.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Ingredient do
-
+  permit_params :description, :drink_id
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
@@ -14,5 +14,5 @@ ActiveAdmin.register Ingredient do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  resources :ingredients
-  resources :drinks
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  scope '/api' do
+    resources :drinks
+  end
 end
